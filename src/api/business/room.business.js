88ececd7ebf2885c9e8b7f530e188be43735c9c7
Rick Droogh <rickdroogh@hotmail.com>
2018-05-15 00:44:10 +0200

    added booking callback

--- src/api/business/room.business.js
+++ src/api/business/room.business.js
@@ -1,5 +1,5 @@
 const {
-  Room, mongoQueryBuilder, getSearchValues, mongoProjectionBuilder,
+  Room, Booking, mongoQueryBuilder, getSearchValues, mongoProjectionBuilder,
 } = require('../models/room.model');
 
 const getFilters = (query) => {
@@ -45,9 +45,32 @@ const list = query =>
       console.error(err);
     });
 
+const createReservation = (postData) => {
+  const booking = new Booking(postData);
+
+  booking.validate(error => Promise.reject(error));
+
+  return Room.findOneAndUpdate(
+    { _id: postData.roomId },
+    { $push: { bookings: booking } },
+    { new: false },
+  )
+    .then((data) => {
+      if (data === null) {
+        throw new Error('No data');
+      }
+      console.log(data);
+    })
+    .catch((error) => {
+      console.error(error);
+      return { status: false, message: 'Something went wrong!' };
+    });
+};
+
 
 module.exports = {
   getFilters,
   list,
   getNames,
+  createReservation,
 };

3d8d5859b1317109f08d1eb7295a896062ca75d4
Daan Grashoff <daan_donn@hotmail.com>
2018-06-19 07:21:18 +0200

    intergration test users groups

---
+++ test/intergration/userGroup.test.js
@@ -0,0 +1,126 @@
+try {
+  if(!init) {
+    require('../../server');
+  }
+} catch (err) {
+  require('../../server');
+}
+
+
+init = true;
+
+
+
+const sinon = require('sinon');
+const assert = require('assert');
+
+const business = require('../../src/api/business/user.business');
+const groupRepository = require('../../src/api/repositories/GroupRepository');
+const notificationHandler = require('../../src/api/utils/notificationHandler');
+const Logger = require('../../src/api/utils/Logger');
+
+describe('userGroup tests', () => {
+  before(() => {
+    sinon
+      .stub(notificationHandler, 'sendToDevice')
+      .callsFake(() => {
+        return {};
+      })
+    sinon
+      .stub(Logger, 'info')
+      .callsFake(() => {
+      });
+  });
+
+  describe('user create and update', () => {
+    let createdUserId = '';
+
+    const createUserObject = {
+      name: 'test-Daan',
+      email: 'test-Daan_donn@hotmail.com',
+      role: 'Student',
+      short: 'test-d',
+    }
+
+    const updateUserObject = {
+      name: 'test-Daan-updated',
+      email: 'test-Daan_donn@hotmail.com',
+      role: 'Student',
+      short: 'test-d',
+    }
+
+    it('should create a user', () => {
+      return business.create(createUserObject)
+      .then((user) => {
+        createdUserId = user._id;
+        assert(user.name === createUserObject.name);
+        assert(user.email === createUserObject.email);
+        assert(user.role === createUserObject.role);
+        assert(user.short === createUserObject.short);
+      });
+    })
+
+    it('should update the user', () => {
+      return business.update(createdUserId, updateUserObject)
+        .then((user) => {
+          assert(user.name === updateUserObject.name);
+          assert(user.email === updateUserObject.email);
+          assert(user.role === updateUserObject.role);
+          assert(user.short === updateUserObject.short);
+        })
+    })
+
+    it('should remove the user', () => {
+      return business.remove(createdUserId)
+        .then((removed) => {
+          assert(removed)
+        })
+    })
+  });
+
+
+  describe('Group create and update', () => {
+    let createGroupId = '';
+
+    const createGroupObject = {
+      name: 'test-Group',
+      description: 'test-group-description',
+    };
+
+    const createGroupObjectUpdate = {
+      name: 'test-Group2.0',
+      description: 'test-group-description2.0',
+    };
+
+    it('should create a group', () => {
+      return groupRepository.create(createGroupObject)
+      .then((group) => {
+        createGroupId = group._id;
+        assert(group.name === createGroupObject.name);
+        return assert(group.description === createGroupObject.description);
+      });
+    })
+
+    it('should update the group', () => {
+      return groupRepository.update(createGroupId, createGroupObjectUpdate)
+        .then((group) => {
+          assert(group.name === createGroupObjectUpdate.name);
+          return assert(group.description === createGroupObjectUpdate.description);
+        })
+    })
+
+    it('should remove the group', () => {
+      return groupRepository.remove(createGroupId)
+        .then((removed) => {
+          return assert(removed)
+        })
+    })
+  });
+
+
+
+  after(() => {
+    Logger.info.restore();
+    notificationHandler.sendToDevice.restore();
+  });
+});
